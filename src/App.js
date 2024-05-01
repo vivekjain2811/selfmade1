9e937ba9d5a178c1108d179d5f9d1a43521f2757
vivek jain <vivekjain.vj.2019@gmail.com>
2024-05-01 21:14:13 +0530

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const[value,setvalue]=useState(0);
+  function increment(){
+setvalue(value+1);
+  }
+  function decrement(){
+    setvalue(value-1);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="flex flex-col w-[100vw] h-[100vh] items-center justify-center">
+      <div className='flex  justify-center text-2xl'><p> INCREMENT AND DECREMENT</p></div>
+      <div className='flex flex-row justify-center  '>
+<button   onClick={increment} className='border-r-2 p-4 '>+</button>
+<span className='p-4'>{value}</span>
+<button onClick={decrement} className='border-l-2 p-4'>-</button>
+</div>
+       
+       
+       
+        </div>
   );
 }
 
